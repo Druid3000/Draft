376431c4f1c761b31570431c0ded5c4c7d43bcb8
Sarvarov, Sergei <Sergei.Sarvarov@Clarivate.com>
2020-07-29 21:28:49 +0300

    [SpringWebFlux] Add comment

--- SpringWebFlux/src/main/java/app/Application.java
+++ SpringWebFlux/src/main/java/app/Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application  {
 
+    // Приложение работает, если его поднять отдельно от этого проекта. Видимо, проблема
+    // заключается в том,что проект тянет настройки общего pom.xml
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
