2a90365dbcdb23ed7afac6b8825d93c620c67074
Druid <sarvarov.s@mail.ru>
2024-02-05 01:39:04 +0300

    [Java8] Edit Stream1.java

--- Java8/src/main/java/streams/attempt1/Stream1.java
+++ Java8/src/main/java/streams/attempt1/Stream1.java
@@ -6,6 +6,8 @@ import java.util.stream.Collectors;
 
 public class Stream1 {
     public static void main(String[] args) {
-        List<String> list = Arrays.stream(args).filter(s-> s.length()<=2).collect(Collectors.toList());
+        List<String> list = Arrays.stream(args)
+                .filter(s-> s.length()<=2)
+                .collect(Collectors.toList());
     }
 }
